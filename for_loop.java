9562d520a93d7f6494ad2f31b5678f9e348df143
Nishant Kumar <157512097+nishant796@users.noreply.github.com>
2025-09-15 10:18:37 +0530

    Intro to for loop

---
+++ for_loop.java
@@ -0,0 +1,7 @@
+public class for_loop {
+    public static void main ( String[] args) {
+        for ( int counter = 0; counter < 10; counter = counter +1) {
+            System.out.println("hello");
+        }
+    }
+}
