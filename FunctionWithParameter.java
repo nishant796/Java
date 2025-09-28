3508c26363646a0b0fbe84704d22d0976ee07000
Nishant Kumar <157512097+nishant796@users.noreply.github.com>
2025-09-28 10:12:25 +0530

    Function with Parameter

---
+++ FunctionWithParameter.java
@@ -0,0 +1,10 @@
+public class FunctionWithParameter {
+    static void greet ( String name ) {
+        // Function with parameters
+        System.out.println("Hello " + name );
+    }
+    public static void main ( String[] args) {
+        greet("Nishant"); // calling with argument
+        greet("Amity");
+    }
+}
