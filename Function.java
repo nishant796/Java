c585235563f22368cd168a29e1a7e2a0c223ae64
Nishant Kumar <157512097+nishant796@users.noreply.github.com>
2025-09-28 10:05:09 +0530

    A simple function without parameters

---
+++ Function.java
@@ -0,0 +1,9 @@
+public class Function {
+    static void sayHello() {
+        System.out.println("Hello, Java!");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
